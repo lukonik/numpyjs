5bc241a9e9d9985b2d5cdd701a289d99a934782e
luka onik <luka.onikadze@toptal.com>
2025-08-25 21:28:12 +0400

    fix: export to index

--- numpyjs/src/index.ts
+++ numpyjs/src/index.ts
@@ -1,1 +1,13 @@
-export * from './lib/numpyjs.js';
+// Core classes and types
+export { Ndarray } from './lib/core/ndarray.js';
+export { DTypes } from './lib/core/dtypes.js';
+
+// Array creation operations
+export { ones } from './lib/operations/ones.js';
+export { zeros } from './lib/operations/zeros.js';
+export { empty } from './lib/operations/empty.js';
+export { eye } from './lib/operations/eye.js';
+export { identity } from './lib/operations/identity.js';
+
+// Utilities
+export { forEachIndex, calculateSize, validateShape } from './lib/utils/ndarray-utils.js';
--- numpyjs/src/lib/numpyjs.spec.ts
+++
@@ -1,7 +0,0 @@
-import { numpyjs } from './numpyjs.js';
-
-describe('numpyjs', () => {
-  it('should work', () => {
-    expect(numpyjs()).toEqual('numpyjs');
-  });
-});
